37913bf6c7b7ed146850455f9ce129dbcdc560a3
Jinho Lee <jinho.lee@ridm.tech>
2025-07-15 15:14:42 +0800

    A bit of clean up

--- include/doda_simulator.hpp
+++ include/doda_simulator.hpp
@@ -3,9 +3,53 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <cassert>
 #include "VDODA.h"
 #include "verilated.h"
-#include "mapper.hpp"
+
+// Helper function for hardware parameter calculations
+static int log2Ceil(int x) {
+    assert(x > 0);
+    int result = 0;
+    x--;
+    while (x > 0) {
+        x >>= 1;
+        result++;
+    }
+    return result;
+}
+
+// Hardware parameters for DODA processor
+class General_Params {
+public:
+    int prog_mem_width;
+    int data_width;
+    int num_pe_per_cluster;
+    int num_cluster;
+    int opcode_width;
+    int inst_tab_size;
+    int data_mem_size_byte;
+    int num_data_mem_entries;
+    int src_pe_idx_width;
+    int src_cluster_idx_width;
+    int src_idx_width;
+
+    // DON'T CHANGE THESE DEFAULT VALUES. THEY ARE MATCHED WITH THE RTL DESIGN.
+    General_Params()
+        : prog_mem_width(128), data_width(32), num_pe_per_cluster(32), num_cluster(4),
+            opcode_width(5), inst_tab_size(256),
+            data_mem_size_byte(1024)
+    {
+        num_data_mem_entries = data_mem_size_byte * 8 / data_width;
+        src_pe_idx_width = log2Ceil(num_pe_per_cluster);
+        src_cluster_idx_width = num_cluster; 
+        src_idx_width = src_pe_idx_width + src_cluster_idx_width;
+
+        assert((num_cluster & (num_cluster - 1)) == 0 && "num_cluster must be a power of 2");
+        assert((num_pe_per_cluster & (num_pe_per_cluster - 1)) == 0 && "num_pe_per_cluster must be a power of 2");
+    }
+    // DON'T CHANGE THESE DEFAULT VALUES. THEY ARE MATCHED WITH THE RTL DESIGN.
+};
 
 class DODASimulator {
 public:
